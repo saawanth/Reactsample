df2a1ce6e7bd9670c04cab9e4db26496781199cf
saawanth <saawanth.j@gmail.com>
2022-02-25 20:14:12 -0600

    04 accordion finish

--- 04-accrodion/src/App.js
+++ 04-accrodion/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
 
 function App() {
+
+  const [question, setQuestion] = useState(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <main>
+     <div className='container'>
+       <h3> accordian module</h3>
+       <section className='info'>
+         {
+           question.map((question) => {
+             return <SingleQuestion key={question.id} {...question} />;
+           })
+         }
+       </section>
+     </div>
+   </main>
   );
 }
 
