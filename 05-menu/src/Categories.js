dacd801d30d6464521fea3915dd36680db1bbc34
saawanth <saawanth.j@gmail.com>
2022-02-25 21:10:14 -0600

    05 menu full

---
+++ 05-menu/src/Categories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Categories = ({filterItems, categories}) => {
+    return (
+        <div className='btn-container'>
+            {
+                categories.map((category) => {
+                   return ( <button type='button' className='filter-btn' onClick={() => filterItems(category)}>{category} </button> );
+                })
+            }
+        </div>
+    );
+};
+
+export default Categories;
